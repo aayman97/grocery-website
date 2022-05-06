aa4daf0db60e95bbdcad161ed21812c5d0023cad
aayman_97 <ahmedayman.ayman80@gmail.com>
2022-05-06 03:18:42 +0200

    Add responsivness to the navbar and home page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <nav>
     <img src="./assets/logo_2.png" />
-    <div class="routers-link-conatiner">
-      <router-link to="/">Home</router-link>
-      <router-link to="/shop">Shop</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/contact">Contact</router-link>
+    <div
+      :style="open ? 'left:0%' : 'left : 100%'"
+      class="routers-link-conatiner"
+    >
+      <router-link @click="closeDrawer" to="/">Home</router-link>
+      <router-link @click="closeDrawer" to="/shop">Shop</router-link>
+      <router-link @click="closeDrawer" to="/about">About</router-link>
+      <router-link @click="closeDrawer" to="/contact">Contact</router-link>
 
       <button>Sign Up</button>
+    </div>
+    <div @click="toggleDrawer" class="drawer">
+      <fa :icon="open ? 'xmark' : 'bars'" />
     </div>
   </nav>
   <router-view />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.open = !this.open;
+      console.log(this.open);
+    },
+    closeDrawer() {
+      this.open = false;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import "scss/nav-bar.scss";
